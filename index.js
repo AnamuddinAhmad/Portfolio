a757a3d2cf074585a22413feca9505985a7543a0
Anamuddin Ahmad <119073478+AnamuddinAhmad@users.noreply.github.com>
2023-04-15 13:23:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// link="https://unpkg.com/typed.js@2.0.15/dist/typed.umd.js">
+
+//   var typingEffect = new Typed(".multitext", {
+//     strings: ["Coder", "Web Developer", "Java Developer"],
+//     loop: true,
+//     typeSpeed: 100,
+//     backSpeed: 80,
+//     backDelay: 1500,
+//   });
+
+// Set the width of the progress bar to 50%
+// var bar = document.getElementById("myBar");
+// var width = 0;
+// var id = setInterval(frame, 10);
+
+// function frame() {
+//     if (width >= 100) {
+//         clearInterval(id);
+//     } else {
+//         width++;
+//         bar.style.width = width + "%";
+//         bar.innerHTML = width + "%";
+//     }
+// }
